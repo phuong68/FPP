44094bf3eba99966be6ef0b95e9175c63927868a
phuong68 <36867014+phuong68@users.noreply.github.com>
2018-03-13 15:27:35 -0500

    done 8.4 & 8.5

--- src/Lesson8_2/Marketing.java
+++ src/Lesson8_2/Marketing.java
@@ -1,5 +1,64 @@
 package Lesson8_2;
 
-public class Marketing {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
+public class Marketing implements Comparator<Marketing>{
+	String employeename;
+	String productname;
+	double salesamount;
+	
+	public Marketing(String employeename, String productname, double salesamount) {
+		super();
+		this.employeename = employeename;
+		this.productname = productname;
+		this.salesamount = salesamount;
+	}
+	
+	
+	
+	@Override
+	public String toString() {
+		return "[" + employeename + " " + productname + " "	+ salesamount + "]";
+	}
+
+
+	@Override
+	public int compare(Marketing o1, Marketing o2) {
+		
+		return Double.compare(o1.salesamount, o2.salesamount);
+	}
+	
+	
+	public int equals(Marketing o2) {
+		
+		return Double.compare(this.salesamount, o2.salesamount);
+	}
+	
+	
+	public static void main(String[] agr) {
+		ArrayList<Marketing> mList = new ArrayList<Marketing>();
+		
+		mList.add(new Marketing("Jack","Coke", 10000.0));
+		mList.add(new Marketing("Hanna","Pepsi", 9900.0));
+		mList.add(new Marketing("Jessica","Sprite", 5000.0));
+		mList.add(new Marketing("Mike","Soda", 3000.0));
+		mList.add(new Marketing("Howie","Dr. Pepper", 1000.0));
+		
+		System.out.println("Unsorted Marketing List");
+		System.out.println(mList);
+		
+		System.out.println("Sorted Marketing List");
+		System.out.println(Collections.sort());
+
+
+
+	
+
+
+
+	
+	
+	
 }
