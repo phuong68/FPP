3b7d597d598fe08d2f410ab5f9f8cf704bcb900b
user <phuong.lehoang@gmail.com>
2018-03-13 23:15:00 -0500

    Lesson2 done

--- src/Lesson8_2/Marketing.java
+++ src/Lesson8_2/Marketing.java
@@ -9,6 +9,12 @@ public class Marketing implements Comparator<Marketing>{
 	String productname;
 	double salesamount;
 	
+	public Marketing() {
+		super();
+	}
+
+
+
 	public Marketing(String employeename, String productname, double salesamount) {
 		super();
 		this.employeename = employeename;
@@ -36,26 +42,59 @@ public class Marketing implements Comparator<Marketing>{
 		return Double.compare(this.salesamount, o2.salesamount);
 	}
 	
+	public static ArrayList<Marketing> listMoreThan1000(ArrayList<Marketing> list){
+		ArrayList<Marketing> retList = new ArrayList<Marketing>();
+		
+		for(Marketing mkt:list) {
+		 if (mkt.salesamount>1000)
+			 retList.add(mkt);
+		}
+		
+		return retList;
+		
+	}
+	
 	
 	public static void main(String[] agr) {
 		ArrayList<Marketing> mList = new ArrayList<Marketing>();
+		Marketing jessie = new Marketing("Jessica","Sprite", 2000.0);
 		
-		mList.add(new Marketing("Jack","Coke", 10000.0));
+		mList.add(new Marketing("Jack","Coke", 1500.0));
 		mList.add(new Marketing("Hanna","Pepsi", 9900.0));
-		mList.add(new Marketing("Jessica","Sprite", 5000.0));
+		mList.add(jessie);
 		mList.add(new Marketing("Mike","Soda", 3000.0));
 		mList.add(new Marketing("Howie","Dr. Pepper", 1000.0));
 		
 		System.out.println("Unsorted Marketing List");
 		System.out.println(mList);
 		
-		System.out.println("Sorted Marketing List");
-		System.out.println(Collections.sort());
+		System.out.println("\nSorted Marketing List");
+		Collections.sort(mList, new Marketing());
+		System.out.println(mList);
+		
+		System.out.println("\nRemove Jessica From Marketing List");
+		mList.remove(jessie);
+		System.out.println(mList);
+		
+		System.out.println("\nSet Jessica to Marketing List at third position");
+		mList.set(2, jessie);
+		System.out.println(mList);
+		
+		System.out.println("\nFilter which Marketting has sales more than 1000");
+		ArrayList<Marketing> list1k =  listMoreThan1000(mList);
+		System.out.println(list1k);
+		
+		System.out.println("\nSorted has-sale-more-than-1000 MarketingList");
+		Collections.sort(list1k, new Marketing());
+		System.out.println(list1k);
+		
+		
+		
 
 
 
 	
-
+	}
 
 
 	
