8690f33f69219bae652881fe30f7c809c5fdfbea
phuong68 <36867014+phuong68@users.noreply.github.com>
2018-03-17 11:57:46 -0500

    Add lesson 12

---
+++ src/Lesson12_1/MarkException.java
@@ -0,0 +1,13 @@
+package Lesson12_1;
+
+/*Write your program to get the input of mark in the range of 0 to 100 from the
+console. Apply the necessary Exception handling to avoid negative, nonnumbers.*/
+public class MarkException {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Lesson12_2/CustomerAccount.java
@@ -0,0 +1,17 @@
+package Lesson12_2;
+
+/*Create a class called CustomerAccount with the following attributes and methods.
+Attributes : Cus_name, Acc_No, Balance.
+Functions : deposit() – Used to increase the balance
+ Withdraw() – Used to reduce the balance.
+Create a user defined exception to handle the following situation
+1. Withdraw amount exceed the balance
+2. Balance reach below 100$.*/
+public class CustomerAccount {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
